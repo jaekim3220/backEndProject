1bcdf7ddec2d58d891f642b4e89e94c70f90b394
jaekim3220 <jehyunkim9@gmail.com>
2024-11-22 23:39:19 +0900

    jaekim3220/backEndProject #2 환자/의사 별로 서로 다른 회원가입 화면 마크업 구현 => TODO : 로그인 페이지, 의료진 소개 페이지 마크업

---
+++ src/main/java/com/backend/doctor/DoctorController.java
@@ -0,0 +1,29 @@
+package com.backend.doctor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
+*/
+
+/*
+<Response 방법 - 서버 기준>
+@Controller + return String => ViewResolver => HTML 파일 렌더링(Model) => HTML
+@Controller + @ResponseBody return String => HTTPMessageConverter => HTML
+@Controller + @ResponseBody return 객체(map, list) => HTTPMessageConverter => jackson => JSON
+@RestController return map => JSON
+Model은 HTML일 경우 사용(@ResponseBody일 경우 Model 사용 불가)
+*/
+
+@Controller
+public class DoctorController {
+
+	
+	// 의사 회원가입 화면
+	@GetMapping("/doctor/sign-up-view")
+	// http:localhost/doctor/sign-up-view
+	public String signUpView() {
+		return "doctor/doctorSignUp";
+	}
+}
