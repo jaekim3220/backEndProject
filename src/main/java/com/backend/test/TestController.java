d489528479e712e356791c1283b3d217e72f4d2f
jaekim3220 <jehyunkim9@gmail.com>
2024-11-22 15:12:48 +0900

    JAVA STS 환경구축 `2. Map @ResponseBody => JSON` 동작 확인

--- src/main/java/com/backend/test/TestController.java
+++ src/main/java/com/backend/test/TestController.java
@@ -1,5 +1,8 @@
 package com.backend.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +22,15 @@ public class TestController {
 	// http:localhost:8080/test2
 	public String test2() {
 		return "<h1>HTML 테스트</h1>";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test3")
+	// http:localhost:8080/test3
+	public Map<String, Object> test3() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", 111);
+		map.put("b", 222);
+		return map;
 	}
 }
