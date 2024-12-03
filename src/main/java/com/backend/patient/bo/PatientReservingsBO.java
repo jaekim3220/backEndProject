102f272814c339eebcdc031474dd1789339981d2
jaekim3220 <jehyunkim9@gmail.com>
2024-12-03 14:12:39 +0900

    jaekim3220/backEndProject #10 `reservings` 테이블에 데이터 INSERT 불가에러 해결 reservings.treatment 컬럼의 NOT NULL로 DEFAULT인 '진료대기'를 INSERT 하도록 설정 => TODO : `reservings` 테이블과 `reservers` 테이블 동시에 UPDATE 기능 구현

--- src/main/java/com/backend/patient/bo/PatientReservingsBO.java
+++ src/main/java/com/backend/patient/bo/PatientReservingsBO.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.backend.common.FileManagerService;
-import com.backend.patient.entity.ReserversEntity;
 import com.backend.patient.entity.PatientReservingsEntity;
-import com.backend.patient.repository.ReserversRepository;
 import com.backend.patient.repository.PatientReservingsRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -54,6 +52,7 @@ public class PatientReservingsBO {
 				.visitDate(visitDate)
 				.imagePath(imagePath)
 				.status("예약대기")
+				.treatment("진료대기")
 				.build();
 		
 		log.error("[!!!!! savedReservingsEntity : {} !!!!!] : ", reservings);
