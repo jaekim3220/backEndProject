5020563c1c199690f5b8f54607b7bd26ae721b82
jaekim3220 <jehyunkim9@gmail.com>
2024-12-04 21:15:21 +0900

    jaekim3220/backEndProject #13  의사가 환자 데이터 변경 시 환자 영역의 `reservers` 테이블이 update 되도록  XML, Mapper, BO 코드 구축 => TODO : 의사 영역의 `reserveings` 테이블 업데이트 되도록 XML, Mapper, BO 파일 구축

---
+++ src/main/java/com/backend/doctor/bo/PatientReserversBO.java
@@ -0,0 +1,36 @@
+package com.backend.doctor.bo;
+
+import org.springframework.stereotype.Service;
+
+import com.backend.doctor.mapper.PatientReserversMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/*
+DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
+*/
+
+// Service(BO)영역
+
+@Slf4j
+@Service // Spring Bean 등록
+@RequiredArgsConstructor
+public class PatientReserversBO {
+
+	
+	// 생성자를 사용한 의존성 주입(DI)
+	private final PatientReserversMapper patientReserversMapper;
+	
+	
+	// input : customerId, doctorId
+	// output : int
+	// @PostMapping("/statusUpdate")
+	public int updateReserversByCustomerIdDoctorId(
+			int id, int customerId, int doctorId, String status) {
+		log.info("##### id={}, customerId={}, doctorId={}, status={} #####", id, customerId, doctorId, status);
+		return patientReserversMapper.updateReserversByIdCustomerIdDoctorId(id, customerId, doctorId, status);
+	}
+	
+	
+}
---
+++ src/main/java/com/backend/doctor/mapper/PatientReserversMapper.java
@@ -0,0 +1,24 @@
+package com.backend.doctor.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/*
+DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper, XML) <--> DB영역 
+*/
+
+// Repository영역(Mapper, XML)
+
+@Mapper // mybatis 라이브러리
+public interface PatientReserversMapper {
+
+	
+	// input : customerId, doctorId, status
+	// output : int
+	// @PostMapping("/statusUpdate")
+	public int updateReserversByIdCustomerIdDoctorId(
+			@Param("id") int id, @Param("customerId") int customerId, 
+			@Param("doctorId") int doctorId, @Param("status") String status);
+	
+	
+}
