93714e0d85d17919bc8a7a3faf39e829413e4fff
jaekim3220 <jehyunkim9@gmail.com>
2024-12-15 12:46:10 +0900

    jaekim3220/backEndProject #16 `reservers` 테이블, `payments` 테이블의 데이터를 둘 다 상위 BO(reserversPayBO)에서 가져오도록 수정 => TODO : ` controller`의 @GetMapping("/reserve-detail-view")에서  HTML로 전송한 Model 데이터 수령 확인 후 HTML에서 PostOne API로 결제 취소 창 활성화 기능 구현

--- src/main/java/com/backend/patient/repository/PaymentsRepository.java
+++ src/main/java/com/backend/patient/repository/PaymentsRepository.java
@@ -11,5 +11,12 @@ DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> 
 // Repository영역(Mapper, XML) 개념
 
 public interface PaymentsRepository extends JpaRepository<PaymentsEntity, Integer> {
-
+	
+	// 메서드 생성
+	
+	// input : int id, int customerId
+	// output : PaymentsEntity
+	// @GetMapping("/reserve-detail-view")
+	PaymentsEntity findByIdAndCustomerId(int id, int customerId);
+	
 }
